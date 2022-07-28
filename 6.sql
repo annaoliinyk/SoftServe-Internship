5f8eaf152bbec49ca73842a607cae906811d4c32
annaoliinyk <anoliinyk@softserveinc.com>
2022-07-28 15:33:34 +0200

    Fixed the format

--- 6.sql
+++ 6.sql
@@ -1,35 +1,25 @@
+-- Show all vendors which are in Alberta and Manitoba
 -- for vendors
-select A.vendor_id, C.province_name 
-  from vendors A
-   join provinces C
-    on A.province_id = C.province_id
- where C.province_name = 'Manitoba'
-  
-  union all
-  
-select A.vendor_id, C.province_name
-  from vendors A 
-   join provinces C
-    on A.province_id = C.province_id
- where C.province_name = 'Alberta'
-
-order by C.province_name  
+SELECT
+  A.vendor_id, 
+  C.province_name 
+ FROM vendors A
+  JOIN provinces C
+   ON A.province_id = C.province_id
+ WHERE C.province_name
+  IN ('Manitoba', 'Alberta')
+ORDER BY C.province_name  
 
 
 -- for patients (out of task, has patients from those provinces)
-select A.first_name, A.last_name, B.province_name 
-  from patients A
-   join provinces B
-    on A.province_id = B.province_id
- where B.province_name = 'Manitoba'
-  
-  union all
-  
-select A.first_name, A.last_name, B.province_name
-  from patients A 
-   join provinces B
-    on A.province_id = B.province_id
- where B.province_name = 'Alberta'
-
-order by B.province_name     
+SELECT 
+  A.first_name, 
+  A.last_name, 
+  B.province_name 
+ FROM patients A
+  JOIN provinces B
+   ON A.province_id = B.province_id
+ WHERE B.province_name
+  IN ('Manitoba', 'Alberta')
+ORDER BY B.province_name             
 
