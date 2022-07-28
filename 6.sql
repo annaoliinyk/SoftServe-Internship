b2015a8b52edbecff99e9603275310042b11af53
annaoliinyk <anoliinyk@softserveinc.com>
2022-07-28 15:50:01 +0200

    Fixed format

--- 6.sql
+++ 6.sql
@@ -1,25 +1,25 @@
 -- Show all vendors which are in Alberta and Manitoba
 -- for vendors
 SELECT
-  A.vendor_id, 
-  C.province_name 
- FROM vendors A
-  JOIN provinces C
-   ON A.province_id = C.province_id
- WHERE C.province_name
+  a.vendor_id, 
+  c.province_name 
+ FROM vendors a
+  JOIN provinces c
+   ON a.province_id = c.province_id
+ WHERE c.province_name
   IN ('Manitoba', 'Alberta')
-ORDER BY C.province_name  
+ORDER BY c.province_name  
 
 
 -- for patients (out of task, has patients from those provinces)
 SELECT 
-  A.first_name, 
-  A.last_name, 
-  B.province_name 
- FROM patients A
-  JOIN provinces B
-   ON A.province_id = B.province_id
- WHERE B.province_name
+  a.first_name, 
+  a.last_name, 
+  b.province_name 
+ FROM patients a
+  JOIN provinces b
+   ON a.province_id = b.province_id
+ WHERE b.province_name
   IN ('Manitoba', 'Alberta')
-ORDER BY B.province_name             
+ORDER BY b.province_name             
 
